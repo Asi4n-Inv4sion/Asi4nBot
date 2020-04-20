7f3868b4cffd0577c4f8be601de2825652147dfe
Leo Wang <leo.wang142001@gmail.com>
2020-04-20 15:20:00 -0400

    Math_Commands.py

--- cogs/Math_Commands.py
+++ cogs/Math_Commands.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 import wolframalpha
 import os
+import time
 
 # Load WolframAlpha AppID
 f = open(os.path.join('D:/Documents/Programming/WolframAppID.txt'))  # BotToken.txt contains the private token
@@ -20,7 +21,7 @@ class Math_Commands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    @commands.command(aliases=['wolfram', 'wr', 'q'])
     async def query(self, ctx, *, q):
         global wolfram_client
         res = wolfram_client.query(q)
